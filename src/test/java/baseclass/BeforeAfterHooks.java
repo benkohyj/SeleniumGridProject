220fd0f63feb4cfbd4d4b8ea37c68ea95074c538
senghwee <senghwee@NI-CNU409DTV0.users.sphinet.com.sg>
2020-02-07 10:41:16 +0800

    Integrated batch files

--- src/test/java/baseclass/BeforeAfterHooks.java
+++ src/test/java/baseclass/BeforeAfterHooks.java
@@ -13,28 +13,34 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import loadscrcpy.Load_scrcpy;
+import setup.CMD;
 
 public class BeforeAfterHooks {
-	
+
 	public AndroidDriver<AndroidElement> androidDriver;
 	public WebDriver browserDriver;
 	public String automationType = ""; 
-	
+
+	@BeforeSuite
+	public void runSetup() throws IOException, InterruptedException {
+		CMD.startHUB();
+	}
+
 	@BeforeTest(alwaysRun=true)
 	@Parameters({"browserName","port","deviceName","udid","systemPort","app","appPackage","appActivity"})
 	public void deviceCaps(String browserName, @Optional String port, 
 			@Optional String deviceName, @Optional String udid, @Optional String systemPort,
-			@Optional String app, @Optional String appPackage, @Optional String appActivity) throws IOException {
-		
+			@Optional String app, @Optional String appPackage, @Optional String appActivity) throws IOException, InterruptedException {
+
 		String rPath= System.getProperty("user.dir"); //User Directory
-		
+
 		if(port==null) {
 			port= "4444";
 		}
@@ -42,81 +48,82 @@ public class BeforeAfterHooks {
 		switch (browserName.toUpperCase()) {
 
 		case("CHROME"):
-			DesiredCapabilities chromeCaps = new DesiredCapabilities();
-			chromeCaps.setBrowserName("chrome");
-			chromeCaps.setPlatform(Platform.ANY);	
-		
-			ChromeOptions chromeoptions = new ChromeOptions();
-			chromeoptions.addArguments("--window-position=0,0");
-			chromeoptions.addArguments("--window-size=800,450");
-			chromeoptions.merge(chromeCaps);
-			
-			URL chromeURL = new URL ("http://localhost:"+port+"/wd/hub");
-			WebDriver chromeDriver = new RemoteWebDriver(chromeURL,chromeoptions);
-			browserDriver= chromeDriver;
-			automationType= "browser";
-			break;
-			
-			
+			CMD.registerBROWSER(browserName);
+		DesiredCapabilities chromeCaps = new DesiredCapabilities();
+		chromeCaps.setBrowserName("chrome");
+		chromeCaps.setPlatform(Platform.ANY);	
+
+		ChromeOptions chromeoptions = new ChromeOptions();
+		chromeoptions.addArguments("--window-position=0,0");
+		chromeoptions.addArguments("--window-size=800,450");
+		chromeoptions.merge(chromeCaps);
+
+		URL chromeURL = new URL ("http://localhost:"+port+"/wd/hub");
+		WebDriver chromeDriver = new RemoteWebDriver(chromeURL,chromeoptions);
+		browserDriver= chromeDriver;
+		automationType= "browser";
+		break;
+
+
 		case("FIREFOX"):
-			DesiredCapabilities firefoxCaps = new DesiredCapabilities();
-			firefoxCaps.setBrowserName("firefox");
-			firefoxCaps.setPlatform(Platform.ANY);
-			
-//			FirefoxOptions firefoxoptions = new FirefoxOptions();
-//			firefoxoptions.addArguments(arguments)
-//			firefoxoptions.addArguments("--window-size=800,450");
-//			firefoxoptions.merge(firefoxCaps);
-			
-			URL firefoxURL = new URL ("http://localhost:"+port+"/wd/hub");
-			WebDriver firefoxDriver = new RemoteWebDriver(firefoxURL,firefoxCaps);
-			browserDriver = firefoxDriver;
-			firefoxDriver.manage().window().setSize(new Dimension(800,450));
-			firefoxDriver.manage().window().setPosition(new Point(800,0));
-			automationType= "browser";
-			break;
-			
+			CMD.registerBROWSER(browserName);
+		DesiredCapabilities firefoxCaps = new DesiredCapabilities();
+		firefoxCaps.setBrowserName("firefox");
+		firefoxCaps.setPlatform(Platform.ANY);
+
+		//			FirefoxOptions firefoxoptions = new FirefoxOptions();
+		//			firefoxoptions.addArguments(arguments)
+		//			firefoxoptions.addArguments("--window-size=800,450");
+		//			firefoxoptions.merge(firefoxCaps);
+
+		URL firefoxURL = new URL ("http://localhost:"+port+"/wd/hub");
+		WebDriver firefoxDriver = new RemoteWebDriver(firefoxURL,firefoxCaps);
+		browserDriver = firefoxDriver;
+		firefoxDriver.manage().window().setSize(new Dimension(800,450));
+		firefoxDriver.manage().window().setPosition(new Point(800,0));
+		automationType= "browser";
+		break;
+
 		case("ANDROIDLOCAL"):
-			Load_scrcpy.loadscrcpy(udid);
-			DesiredCapabilities androidCaps = new DesiredCapabilities();
-			androidCaps.setCapability("automationName", "UiAutomator2");
-			androidCaps.setCapability("platformName", "Android");
-			androidCaps.setCapability("deviceName", deviceName);
-			androidCaps.setCapability("udid", udid);
-			androidCaps.setCapability("systemPort", systemPort);
-			//androidCaps.setCapability("app", rPath + app); 		//uncomment if app is not installed
-			androidCaps.setCapability("appPackage", appPackage);
-			androidCaps.setCapability("appActivity", appActivity);
-			androidCaps.setCapability("noReset", "false");
-			androidCaps.setCapability("fullReset", "false");
-			
-			URL androidURL = new URL ("http://localhost:"+port+"/wd/hub");
-			androidDriver = new AndroidDriver<AndroidElement>(androidURL,androidCaps);
-			automationType= "android";
-			break;
+			CMD.loadscrcpy(udid);
+		CMD.registerMOBILE(deviceName);
+		DesiredCapabilities androidCaps = new DesiredCapabilities();
+		androidCaps.setCapability("automationName", "UiAutomator2");
+		androidCaps.setCapability("platformName", "Android");
+		androidCaps.setCapability("deviceName", deviceName);
+		androidCaps.setCapability("udid", udid);
+		androidCaps.setCapability("systemPort", systemPort);
+		//androidCaps.setCapability("app", rPath + app); 		//uncomment if app is not installed
+		androidCaps.setCapability("appPackage", appPackage);
+		androidCaps.setCapability("appActivity", appActivity);
+		androidCaps.setCapability("noReset", "false");
+		androidCaps.setCapability("fullReset", "false");
+
+		URL androidURL = new URL ("http://localhost:"+port+"/wd/hub");
+		androidDriver = new AndroidDriver<AndroidElement>(androidURL,androidCaps);
+		automationType= "android";
+		break;
 
 		}
 	}
-	
-	
+
+
 	@AfterTest(alwaysRun=true)
 	public void tearDown() {
 		switch(automationType.toUpperCase()){
-		
+
 		case("BROWSER"): 		
 			browserDriver.quit();
-			System.out.println("Browser Test Ended");
-			break;
-			
+		System.out.println("Browser Test Ended");
+		break;
+
 		case("ANDROID"): 		
 			androidDriver.closeApp();
-			System.out.println("App Closed");
-			androidDriver.quit();
-			System.out.println("Android Test Ended");
-			break;
-				
+		System.out.println("App Closed");
+		androidDriver.quit();
+		System.out.println("Android Test Ended");
+		break;
 		}
-		
 	}
 
 }
---
+++ src/test/java/setup/CMD.java
@@ -0,0 +1,68 @@
+package setup;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CMD {
+
+	private static String userPath= System.getProperty("user.dir"); //User Directory
+	private static File setupFiles= new File(userPath + "/setupFiles"); //directory to setup batch files
+	private static Runtime r=Runtime.getRuntime();
+	private static String startCMD="cmd /c start /min cmd.exe /K ";
+
+	public static void loadscrcpy(String udid)throws IOException{
+		r.exec(startCMD +"scrcpy -s " + udid);
+	}
+
+
+	public static void startHUB() throws IOException, InterruptedException {
+		r.exec(startCMD + "RegisterHub.bat", null, setupFiles);
+		Thread.sleep(5000);
+	}
+
+
+	public static void registerBROWSER(String browser) throws IOException, InterruptedException {
+
+		switch(browser.toUpperCase()) {
+
+		case("CHROME"):
+			r.exec(startCMD + "RegisterChrome.bat", null, setupFiles);
+		break;
+
+		case("FIREFOX"):
+			r.exec(startCMD + "RegisterFirefox.bat", null, setupFiles);
+		break;		
+		
+		}
+		Thread.sleep(5000);
+	}
+
+
+	public static void registerMOBILE(String device) throws IOException, InterruptedException {
+
+		switch(device) {
+
+		case("Samsung Galaxy S9"):
+			r.exec(startCMD + "RegisterS9.bat", null, setupFiles);
+		break;
+
+		case("Samsung Galaxy Note 8"):
+			r.exec(startCMD + "RegisterNoet8.bat", null, setupFiles);
+		break;	
+
+		case("Samsung Galaxy Note 4"):
+			r.exec(startCMD + "RegisterNote4.bat", null, setupFiles);
+		break;	
+		}
+		
+		Thread.sleep(10000);
+	}
+
+//	public static void quitCMD() throws IOException {
+//		r.exec("exit");
+//	}
+
+} 
+
+//Further improvement: Implement scrcpy for MAC/IOS
+
